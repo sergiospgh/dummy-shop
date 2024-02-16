61a4a10e503a5bf76dda86c509fef648a9712956
sergio.serranop <sergio.serranop@alten.es>
2024-02-16 23:22:40 +0100

    feat: added mock endpoints

--- src/app/core/services/products.service.ts
+++ src/app/core/services/products.service.ts
@@ -3,6 +3,7 @@ import { Injectable } from '@angular/core';
 import { CoreModule } from '@app/core.module';
 import { ENDPOINT } from '@shared/constants/endpoints.constants';
 import { ProductsFilter } from '@shared/interfaces/products.interface';
+import { ProductPayload } from '@shared/payloads/product.payload';
 import { GetProductsResponse } from '@shared/responses/products.response';
 import { Observable } from 'rxjs';
 
@@ -15,5 +16,20 @@ export class ProductsService {
   getProducts(filter: ProductsFilter): Observable<GetProductsResponse> {
     const url = ENDPOINT.PRODUCTS.GET_ALL;
     return this.http.get<GetProductsResponse>(url, { params: { ...filter } });
+  }
+
+  addProduct(product: ProductPayload): Observable<void> {
+    const url = ENDPOINT.PRODUCTS.ADD;
+    return this.http.post<void>(url, product);
+  }
+
+  updateProduct(product: ProductPayload): Observable<void> {
+    const url = ENDPOINT.PRODUCTS.UPDATE.replace('{id}', product.id.toString());
+    return this.http.put<void>(url, product);
+  }
+
+  deleteProduct(id: number): Observable<void> {
+    const url = ENDPOINT.PRODUCTS.DELETE.replace('{id}', id.toString());
+    return this.http.delete<void>(url);
   }
 }
--- src/app/shared/constants/endpoints.constants.ts
+++ src/app/shared/constants/endpoints.constants.ts
@@ -2,5 +2,8 @@ export const ENDPOINT = {
   LOGIN: 'https://dummyjson.com/auth/login',
   PRODUCTS: {
     GET_ALL: 'https://dummyjson.com/products',
+    ADD: 'https://dummyjson.com/products/add',
+    UPDATE: 'https://dummyjson.com/products/{id}',
+    DELETE: 'https://dummyjson.com/products/{id}',
   },
 };
